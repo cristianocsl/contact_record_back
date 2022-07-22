5d73ac863dbbb6946a93aa1e139279d7134b9539
cristianocsl <cslcristiano@gmail.com>
2022-07-22 14:12:52 -0300

    refactor: renomeia função de gravar um contato

--- api/models/contact/index.js
+++ api/models/contact/index.js
@@ -1,6 +1,6 @@
 const connection = require('../connection');
 
-module.exports.record = async (contactInformation) => {
+module.exports.recordModel = async (contactInformation) => {
   const db = await connection();
   const { insertedId } = await db.collection('contact').insertOne(contactInformation);
   return { _id: insertedId };
